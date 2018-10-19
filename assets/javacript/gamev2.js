82e8c28e10505af371c023d7a46936b9f28015f0
Zach Merel <zachmerel@gmail.com>
2018-10-18 21:25:38 -0500

    reset fully works with all elements, 2 of 5 sounds play for words as of now.

--- assets/javacript/gamev2.js
+++ assets/javacript/gamev2.js
@@ -3,14 +3,15 @@
 
 var wordBank = ["ENTOURAGE", "GAME OF THRONES", "THE SOPRANOS", "MAD MEN", "RICK AND MORTY"];
 var wins = 0;
+var winTracker = 0;
 var wrongLetter = [];
 var guessesLeft = 9;
 var underScores = [];
-var correctGuesses = [];
 var randomWord;
-var wintracker = 0;
+var winCount = 0;
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
+var GOTtheme = document.getElementById("GOT");
+var madMenTheme = document.getElementById("MadMen");
 
 // Function
 // ==============================================================================
@@ -31,65 +32,102 @@ function startGame() {
     }
     //Displays the underscores
     document.getElementById('wordBlanks').innerHTML = underScores.join(" ");
+    document.getElementById('letters_guessed').innerHTML = wrongLetter;
+    document.getElementById('guesses_left').innerHTML = guessesLeft;
 
-    //resets the variables when the round is over
-    wrongLetter = [];
-    guessesLeft = 9;
-}
-function winlose() {
-    if (wintracker === randomWord.length) {
-        console.log("you win");
-        //display picture of show and title while playing theme music
-    }
-    else if (guessesLeft === 0) {
-        console.log("you lose");
-        //play wahwah sound?
-    }
+
+
 }
 
 // User letter guess
 document.onkeyup = function (event) {
-    var userGuess = event.key.toUpperCase();
-    //Testing to see if user guess is a letter character
-    // if(alphabet.indexOf(userGuess) > -1){ 
-    document.keyCode = function (event) {
-        if (userGuess.keyCode < 65 && userGuess.keyCode > 90) {
-            console.log(keyCode);
-            alert('this is not a valid guess');
-        }
+    console.log(event)
+    if (event.keyCode < 65 || event.keyCode > 90) {
+        console.log(event.keyCode);
+        alert('this is not a valid guess');
     }
-    //tests to see if user's character guess is in the word
-    if (randomWord.indexOf(userGuess) > -1) {
-        for (i = 0; i < randomWord.length; i++) {
-            if (randomWord[i] === userGuess) {
-                underScores[i] = userGuess;
-
-                console.log(underScores)
-                wintracker++;
-                console.log(wintracker);
-            }
-        }
-        document.getElementById('wordBlanks').innerHTML = underScores.join(" ");
-    }
-    //This prevents the user from guessing the same letter mutliple times and alerts them
-    else if (wrongLetter.indexOf(userGuess) > -1) {
-        alert('you have already guessed this letter');
-    }
-
     else {
-        wrongLetter.push(userGuess);
-        guessesLeft--;
-        document.getElementById('guesses_left').innerHTML = guessesLeft;
-        document.getElementById('letters_guessed').innerHTML = wrongLetter;
+        var userGuess = event.key.toUpperCase();
+
+        //tests to see if user's character guess is in the word
+        if (randomWord.indexOf(userGuess) > -1) {
+            for (i = 0; i < randomWord.length; i++) {
+                if (randomWord[i] === userGuess) {
+                    underScores[i] = userGuess;
+
+                }
+            }
+            winLose();
+            document.getElementById('wordBlanks').innerHTML = underScores.join(" ");
+        }
+        //This prevents the user from guessing the same letter mutliple times and alerts them
+        else if (wrongLetter.indexOf(userGuess) > -1) {
+            alert('you have already guessed this letter');
+        }
+
+        else {
+            wrongLetter.push(userGuess);
+            winLose();
+            guessesLeft--;
+            document.getElementById('guesses_left').innerHTML = guessesLeft;
+            document.getElementById('letters_guessed').innerHTML = wrongLetter;
+        }
+
     }
+}
+//determining wins and lose
+function winLose() {
+    console.log(underScores);
+    var testwin = underScores.includes(' _ ');
+
+    if (testwin === false && randomWord == wordBank[1]) {
+        winCount++;
+        console.log("you win");
+        //display picture of show and title while playing theme music adds one to win variable
+        document.getElementById('wins_text').innerHTML = "Wins:" + winCount;
+        playAudioGOT();
+    }
+    else if (testwin === false && randomWord == wordBank[3]) {
+        winCount++;
+        console.log("you win");
+        //display picture of show and title while playing theme music adds one to win variable
+        document.getElementById('wins_text').innerHTML = "Wins:" + winCount;
+        playAudioMadmen();
+    }
+    else if (guessesLeft === 0) {
+        console.log("you lose");
+        //alert user
+    }
+}
+
+//resets the variables when the round is over
+$(".reset").on("click", function () {
+    winTracker = 0;
+    wrongLetter = [];
+    guessesLeft = 9;
+    underScores = [];
+    randomWord;
+    pauseAudioGOT();
+    pauseAudioMadMen();
+    startGame();
+})
+//fucntions to play or pause music when win and reset
+function playAudioGOT() {
+    GOTtheme.play();
+}
+function playAudioMadmen() {
+    madMenTheme.play();
+}
+function pauseAudioGOT() {
+    GOTtheme.pause();
+}
+function pauseAudioMadMen() {
+    madMenTheme.pause();
 }
 
 
 
-
-
-
-
-// Main
+// Calls Functions
 // ==============================================================================
 startGame();
+winLose();
